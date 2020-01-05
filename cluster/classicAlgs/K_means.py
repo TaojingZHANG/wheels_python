7746998b852f003f925c2bf6d217c8c264bc2501
zhangtaojing <tj_zh@hotmail.com>
2020-01-05 10:44:28 +0800

    add K-means algorithm, not finish

--- cluster/classicAlgs/K_means.py
+++ cluster/classicAlgs/K_means.py
@@ -1,10 +1,37 @@
+# from CSDN
 from collections import defaultdict
+import random as ra
 
-def generate_k(data,k):
-    centers=[]
+
+# 随机产生数据域内k个初始簇中心点
+def generate_k(data, k):
+    centers = []
     dimensions = len(data[0])
-    min_max = defaultdict(int)#  字典里的key不存在但被查找时，返回的不是keyError,而是一个默认值int 0
+    min_max = defaultdict(int)  # 创建字典，字典里的key不存在但被查找时，返回的不是keyError,而是一个默认值int 0
 
+    # 找出各个维度的最小/最大的点，即求数据域范围
     for point in data:
         for i in range(dimensions):
-            val=point[i]
+            val = point[i]
+            min_key = 'min_%d' % i  # 用i替换%d (%i 为整体),输出字符串min_i
+            max_key = 'max_%d' % i
+            if min_key not in min_max or val < min_max[min_key]:
+                min_max[min_key] = val
+            if max_key not in min_max or val > min_max[max_key]:
+                min_max[max_key] = val
+
+    # 随机生成k个点
+    for _k in range(k):
+        rand_point = []
+        for i in range(dimensions):
+            min_val = min_max['min_%d' % i]
+            max_val = min_max['max_%d' % i]
+            rand_point.append(ra.uniform(min_val, max_val))  # 在最小和最大范围内取随机点
+        centers.append(rand_point)
+
+    return centers
+
+
+#平方和误差函数
+def distance(a,b):
+    dimensions = len(a)
--- cluster/classicAlgs/showResult.py
+++ cluster/classicAlgs/showResult.py
@@ -1,22 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def show_seed():
-    with open('H:\PRJ14_ECOVACS\cluster\\threeAlgs\seeds_dataset.txt') as f:
-        read_data=f.read()
-        sValues=read_data.split()
-        nValues=[float(x) for x in sValues]
-        seeds_values = np.array(nValues).reshape(210,8)
+    with open('.\seeds_dataset.txt') as f:
+        read_data = f.read()
+        sValues = read_data.split()
+        nValues = [float(x) for x in sValues]
+        seeds_values = np.array(nValues).reshape(210, 8)
     f.closed
-    figure,ax=plt.subplots(6,6)
-    for i in range(0,6):
-        for j in range(0,6):
-            Y=np.array(seeds_values[:,[i,j]]) # 将第i列和第j列放到一个二维array里
-            for l in range(0,210):
+    figure, ax = plt.subplots(6, 6)
+    for i in range(0, 6):
+        for j in range(0, 6):
+            Y = np.array(seeds_values[:, [i, j]])  # 将第i列和第j列放到一个二维array里
+            for l in range(0, 210):
                 ax[i][j].scatter(Y[l][0], Y[l][1], s=1, color='mediumseagreen')
             # print(Y)
             # print("_")
     plt.show()
+
 
 if __name__ == '__main__':
     show_seed()
